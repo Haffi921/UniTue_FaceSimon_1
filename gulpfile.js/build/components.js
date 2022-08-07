dbdf568d27ff225c79bb46b57a1896f47293a862
Hafsteinn Ragnarsson <haffi921@gmail.com>
2022-08-07 22:07:58 +0200

    Changed to @rollup/stream

--- gulpfile.js/build/components.js
+++ gulpfile.js/build/components.js
@@ -1,5 +1,9 @@
 const { resolve } = require("path");
 
+// Vinyl
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+
 // Gulp
 const { parallel, src, dest } = require("gulp");
 const uglify = require("gulp-uglify");
@@ -7,7 +11,7 @@ const replace = require("gulp-replace");
 const rename = require("gulp-rename");
 
 // Rollup
-const rollup = require("gulp-better-rollup");
+const rollup = require("@rollup/stream");
 const { babel } = require("@rollup/plugin-babel");
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
@@ -19,25 +23,23 @@ const cssNano = require("gulp-cssnano");
 const sass = require("gulp-sass")(require("node-sass"));
 const tildeImporter = require("node-sass-tilde-importer");
 
-function javascript(file_path, dest_path) {
+function javascript(entry_name, file_path, dest_path) {
   return function javascript() {
-    return src(file_path)
-      .pipe(
-        rollup(
-          {
-            plugins: [
-              typescript(),
-              babel({
-                babelHelpers: "bundled",
-                exclude: [/node_modules/],
-              }),
-              nodeResolve(),
-              commonjs(),
-            ],
-          },
-          "umd"
-        )
-      )
+    return rollup({
+      input: file_path,
+      output: { format: "cjs" },
+      plugins: [
+        typescript(),
+        babel({
+          babelHelpers: "bundled",
+          exclude: [/node_modules/],
+        }),
+        nodeResolve(),
+        commonjs(),
+      ],
+    })
+      .pipe(source(`${entry_name}.js`))
+      .pipe(buffer())
       .pipe(uglify())
       .pipe(rename("index.js"))
       .pipe(dest(dest_path));
@@ -73,7 +75,7 @@ function html(title, entry_name, dest_path) {
 function createComponent(title, entry_name, entry_path, dest_path) {
   const component_path = resolve(dest_path, entry_name);
   return parallel(
-    javascript(entry_path, component_path),
+    javascript(entry_name, entry_path, component_path),
     css(component_path),
     html(title, entry_name, component_path)
   );
